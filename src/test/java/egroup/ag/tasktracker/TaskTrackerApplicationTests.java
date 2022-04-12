90a258f83d5ea51b5512f50bce3d873d99e303a3
rasulacaldera <rasula.caldera@hotmail.com>
2022-04-12 06:13:09 +0530

    initial commit

---
+++ src/test/java/egroup/ag/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package egroup.ag.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
